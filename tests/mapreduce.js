b17feb858dfe4c1786a21d50bfe2be6d2e49500a
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-09-30 14:08:56 +0200

    Fixed indentation.

--- tests/mapreduce.js
+++ tests/mapreduce.js
@@ -1,8 +1,8 @@
 describe('MapReduce', function() {
-  it('should return {0:[2],1:[4],2:[6],3:[8]} for ([1,2,3,4], (k,v) -> emit(k,v*2))', function() {
-    var output = mapReduce([1,2,3,4], function(key, value, emit) {
-        emit(key, value * 2);
+    it('should return {0:[2],1:[4],2:[6],3:[8]} for ([1,2,3,4], (k,v) -> emit(k,v*2))', function() {
+        var output = mapReduce([1,2,3,4], function(key, value, emit) {
+            emit(key, value * 2);
+        });
+        expect(output).toEqual({0:[2],1:[4],2:[6],3:[8]});
     });
-    expect(output).toEqual({0:[2],1:[4],2:[6],3:[8]});
-  });
 });
