28b99dd72bc9af9333c241ddf9c801697cd6be2a
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-09-30 15:46:16 +0200

    Use closures to encapsulate default behavior into a function called submitJob.

--- ganesha.js
+++ ganesha.js
@@ -1,64 +1,70 @@
 /**
  * @param (data -> []) inputFormat Converts data to an array.
  */
-var ganesha = function(job) {
-    if (!job) {
-        throw 'Please provide a job description.';
-    }
-    if (!job['data']) {
-        throw 'Please provide input data.';
-    }
-    if (!job['map']) {
-        throw 'Please provide a map function.';
-    }
-    if (!job['callback']) {
-        throw 'Please provide a callback function.';
-    }
-    var mapOutput = {};
-    var reduceOutput = {};
-    var mapEmitter = function(key, value) {
-        if (!mapOutput[key]) {
-            mapOutput[key] = [];
+var ganesha = function() {
+    var _submitJob = function(job) {
+        if (!job) {
+            throw 'Please provide a job description.';
         }
-        mapOutput[key].push(value);
-    };
-    var reduceEmitter = function(key, value) {
-        if (reduceOutput[key] === undefined) {
-            reduceOutput[key] = [];
+        if (!job['data']) {
+            throw 'Please provide input data.';
         }
-        reduceOutput[key].push(value);
-    };
-    if (job['inputFormat']) {
-        job['data'] = job['inputFormat'](job['data']);
-    }
-    if (!Array.isArray(job['data'])) {
-        throw 'Please provide the input data as an array.';
-    }
-    job['data'].forEach(function(element, index) {
-        job['map'](index, element, mapEmitter);
-    });
-    if (!job['reduce']) {
-        job['callback'](mapOutput);
-    } else {
-        for (var key in mapOutput) {
-            job['reduce'](key, mapOutput[key], reduceEmitter);
+        if (!job['map']) {
+            throw 'Please provide a map function.';
+        }
+        if (!job['callback']) {
+            throw 'Please provide a callback function.';
+        }
+        var mapOutput = {};
+        var reduceOutput = {};
+        var mapEmitter = function(key, value) {
+            if (!mapOutput[key]) {
+                mapOutput[key] = [];
+            }
+            mapOutput[key].push(value);
         };
-        job['callback'](reduceOutput);
-    }
-};
-ganesha.createJob = function(data, map, callback, options) {
-    var job = {
-        data: data,
-        map: map,
-        callback: callback
+        var reduceEmitter = function(key, value) {
+            if (reduceOutput[key] === undefined) {
+                reduceOutput[key] = [];
+            }
+            reduceOutput[key].push(value);
+        };
+        if (job['inputFormat']) {
+            job['data'] = job['inputFormat'](job['data']);
+        }
+        if (!Array.isArray(job['data'])) {
+            throw 'Please provide the input data as an array.';
+        }
+        job['data'].forEach(function(element, index) {
+            job['map'](index, element, mapEmitter);
+        });
+        if (!job['reduce']) {
+            job['callback'](mapOutput);
+        } else {
+            for (var key in mapOutput) {
+                job['reduce'](key, mapOutput[key], reduceEmitter);
+            };
+            job['callback'](reduceOutput);
+        }
     };
-    if (options) {
-        if (options['reduce']) {
-            job['reduce'] = options['reduce']
+    var _createJob = function(data, map, callback, options) {
+        var job = {
+            data: data,
+            map: map,
+            callback: callback
+        };
+        if (options) {
+            if (options['reduce']) {
+                job['reduce'] = options['reduce']
+            }
+            if (options['inputFormat']) {
+                job['inputFormat'] = options['inputFormat']
+            }
         }
-        if (options['inputFormat']) {
-            job['inputFormat'] = options['inputFormat']
-        }
-    }
-    return job;
-}
+        return job;
+    };
+    return {
+        submitJob: _submitJob,
+        createJob: _createJob
+    };
+}();
--- tests/mapreduce.js
+++ tests/mapreduce.js
@@ -2,32 +2,32 @@ describe('MapReduce', function() {
 
     it('should fail for no arguments', function() {
         expect(function() {
-            ganesha();
+            ganesha.submitJob();
         }).toThrow('Please provide a job description.');
     });
 
     it('should fail for empty job description', function() {
         expect(function() {
-            ganesha(ganesha.createJob())
+            ganesha.submitJob(ganesha.createJob())
         }).toThrow('Please provide input data.');
     });
 
     it('should fail for just the data argument', function() {
         expect(function() {
-            ganesha(ganesha.createJob('test'))
+            ganesha.submitJob(ganesha.createJob('test'))
         }).toThrow('Please provide a map function.');
     });
 
     it('should fail if the input data is not an array and no inputFormat function is provided', function() {
         expect(function() {
-            ganesha(ganesha.createJob('test', function(key, value, emit) {
+            ganesha.submitJob(ganesha.createJob('test', function(key, value, emit) {
                 emit(key, value);
             }, function(output) {}))
         }).toThrow('Please provide the input data as an array.');
     });
 
     it('should return {0:[2],1:[4],2:[6],3:[8]} for ([1,2,3,4], (k,v) -> emit(k,v*2))', function() {
-        var output = ganesha(ganesha.createJob([1,2,3,4], function(key, value, emit) {
+        var output = ganesha.submitJob(ganesha.createJob([1,2,3,4], function(key, value, emit) {
             emit(key, value * 2);
         }, function(output) {
             expect(output).toEqual({0:[2],1:[4],2:[6],3:[8]});
@@ -36,7 +36,7 @@ describe('MapReduce', function() {
 
     it('should return "in" as the most frequent word', function() {
         var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque commodo porta facilisis. Sed pretium velit facilisis enim feugiat ut bibendum orci elementum. Fusce mattis, ante id porta adipiscing, orci mauris ultricies enim, in tincidunt erat mauris eget arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi metus tellus, euismod nec consectetur in, malesuada eget purus. In placerat sapien a purus tempor hendrerit. Quisque vel mi euismod orci tristique auctor non ac nunc. Nam sit amet nunc erat. Mauris dictum augue non lectus tincidunt quis interdum nunc convallis. Suspendisse eleifend, leo at cursus hendrerit, nisl leo faucibus turpis, vitae volutpat massa nisi in erat. Morbi dictum nunc nec erat aliquet adipiscing sed id dolor.";
-        var output = ganesha(ganesha.createJob(text, function(key, value, emit) {
+        var output = ganesha.submitJob(ganesha.createJob(text, function(key, value, emit) {
             emit(value, 1);
         }, function(output) {
             var mostFrequentWord;
