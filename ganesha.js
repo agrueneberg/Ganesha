c711b636b01818f927240fa4c35205da062f9d80
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-10-01 14:11:00 +0200

    Pass JSLint (except for a newcap issue).

--- ganesha.js
+++ ganesha.js
@@ -1,47 +1,15 @@
-var ganesha = function() {
+/*jslint white: true, nomen: true, newcap: true */
+
+var ganesha = (function() {
+    "use strict";
     /**
-     * Submits a job.
+     * Declarations
      */
-    var _submitJob = function(job) {
-        if (!job || job.constructor !== _Job) {
-            throw 'Please provide a job description.';
-        }
-        var mapOutput = {};
-        var reduceOutput = {};
-        var mapEmitter = function(key, value) {
-            if (!mapOutput[key]) {
-                mapOutput[key] = [];
-            }
-            mapOutput[key].push(value);
-        };
-        var reduceEmitter = function(key, value) {
-            if (reduceOutput[key] === undefined) {
-                reduceOutput[key] = [];
-            }
-            reduceOutput[key].push(value);
-        };
-        job['data'].forEach(function(element, index) {
-            job['map'](index, element, mapEmitter);
-        });
-        if (!job['reduce']) {
-            job['callback'](mapOutput);
-        } else {
-            for (var key in mapOutput) {
-                job['reduce'](key, mapOutput[key], reduceEmitter);
-            };
-            job['callback'](reduceOutput);
-        }
-    };
-    /**
-     * Creates a new job.
-     */
-    var _createJob = function(data, map, callback, options) {
-        return new _Job(data, map, callback, options);
-    };
+    var _Job, _submitJob, _createJob;
     /**
      * Job class
      */
-    var _Job = function(data, map, callback, options) {
+    _Job = function(data, map, callback, options) {
         if (!data || !Array.isArray(data)) {
             throw 'Please provide an input data array.';
         }
@@ -54,12 +22,54 @@ var ganesha = function() {
         this.data = data;
         this.map = map;
         this.callback = callback;
-        if (options && options['reduce'] && typeof options['reduce'] === 'function') {
-            this.reduce = options['reduce'];
+        if (options && options.reduce && typeof options.reduce === 'function') {
+            this.reduce = options.reduce;
         }
+    };
+    /**
+     * Submits a job.
+     */
+    _submitJob = function(job) {
+        var mapOutput, reduceOutput, mapEmitter, reduceEmitter, key;
+        if (!job || job.constructor !== _Job) {
+            throw 'Please provide a job description.';
+        }
+        mapOutput = {};
+        reduceOutput = {};
+        mapEmitter = function(key, value) {
+            if (!mapOutput[key]) {
+                mapOutput[key] = [];
+            }
+            mapOutput[key].push(value);
+        };
+        reduceEmitter = function(key, value) {
+            if (reduceOutput[key] === undefined) {
+                reduceOutput[key] = [];
+            }
+            reduceOutput[key].push(value);
+        };
+        job.data.forEach(function(element, index) {
+            job.map(index, element, mapEmitter);
+        });
+        if (!job.reduce) {
+            job.callback(mapOutput);
+        } else {
+            for (key in mapOutput) {
+                if (mapOutput.hasOwnProperty(key)) {
+                    job.reduce(key, mapOutput[key], reduceEmitter);
+                }
+            }
+            job.callback(reduceOutput);
+        }
+    };
+    /**
+     * Creates a new job.
+     */
+    _createJob = function(data, map, callback, options) {
+        return new _Job(data, map, callback, options);
     };
     return {
         submitJob: _submitJob,
         createJob: _createJob
     };
-}();
+}());
